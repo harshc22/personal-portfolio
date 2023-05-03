e7289fa36a0b567de789fde09de1928aa4efaf33
Harsh <harshpreet571@gmail.com>
2023-05-03 15:30:47 -0400

    Added the skills section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useEffect, useState } from 'react';
-import logo from '../assets/img/logo.svg'
+import logo from '../assets/img/logo.png'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
